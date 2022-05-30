349661d7fc04d46beb9c780986fe902a5e7e8799
Taras Demchenko <taras.demchenko@advantiss.com>
2022-05-30 13:26:00 +0300

    bootstrap form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ImageComponentComponent } from './image-component/image-component.component';
 import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ImageComponentComponent,
-    EmpInfoComponent
+    EmpInfoComponent,
+    FormComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
